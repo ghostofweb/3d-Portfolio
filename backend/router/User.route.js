9f9f8156ddc1b70e455abf39edab2221fd546c20
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2026-02-07 02:17:34 +0530

    changes of blogs and auth are complete with otp on email

--- backend/router/User.route.js
+++ backend/router/User.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteUserAccount, forgotPassword, getAllUsers, getUserProfile, loginUser, registerUser, removeMember, resetPassword, sendRegistrationOTP, updateUserProfile } from "../service/User.service.js";
+import { deleteUserAccount, forgotPassword, getAllUsers, getUserProfile, loginUser, registerUser, removeMember, resetPassword, resetPasswordWithOTP, sendCurrentUserOTP, sendPasswordResetOTP, sendRegistrationOTP, updateUserProfile } from "../service/User.service.js";
 import { verifyJWT } from "../middleware/verifyJWT.js";
 
 export const UserRouter = Router();
@@ -11,6 +11,9 @@ UserRouter.put("/update-profile",verifyJWT, updateUserProfile);
 UserRouter.delete("/delete-account",verifyJWT, deleteUserAccount);
 UserRouter.get("/all-users", verifyJWT, getAllUsers);
 UserRouter.delete("/remove-member/:id", verifyJWT, removeMember);
+UserRouter.post("/forgot-password-otp", sendPasswordResetOTP);
+UserRouter.post("/reset-password-otp", resetPasswordWithOTP);
+UserRouter.post("/send-current-user-otp", verifyJWT, sendCurrentUserOTP);
 UserRouter.post("/forgot-password", forgotPassword);
 UserRouter.put("/reset-password/:token", resetPassword);
 UserRouter.post("/send-otp", sendRegistrationOTP);
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,24 +2,22 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    secure: false, // <--- THIS MUST BE FALSE FOR PORT 2525
+    service: 'gmail',
     auth: {
-      user: process.env.MAILTRAP_USER, 
-      pass: process.env.MAILTRAP_PASS 
-    }
+      user: process.env.GMAIL, 
+      pass: process.env.GMAIL_PASSWORD, 
+    },
   });
 
   const mailOptions = {
-    from: '"GhostOfWeb Admin" <noreply@ghostofweb.com>',
+    from: `"GhostOfWeb" <${process.env.GMAIL}>`, 
     to: options.email,
     subject: options.subject,
     text: options.message,
     html: options.html,
   };
 
-  await transporter.sendMail(mailOptions);
+  return await transporter.sendMail(mailOptions)
 };
 
 export default sendEmail;
